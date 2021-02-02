8771369b0f54fdf2cb6594a0363ac6871195425e
Oyxiaoxi <Oyxiaoxi@hotmail.com>
2021-02-02 21:12:51 +0000

    Test View Single Question

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Question;
 
 class QuestionsController extends Controller
 {
     public function index()
     {
+    }
+
+    public function show(Question $question)
+    {
+        return view('questions.show', compact('question'));
     }
 }
