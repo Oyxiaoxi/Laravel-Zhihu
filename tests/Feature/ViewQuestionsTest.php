28e172e058c6676e122d9d75f61ca1e0288d20ae
Oyxiaoxi <Oyxiaoxi@hotmail.com>
2021-02-02 21:34:36 +0000

    Question Published_at Attribute

--- tests/Feature/ViewQuestionsTest.php
+++ tests/Feature/ViewQuestionsTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use App\Models\Question;
+use Carbon\Carbon;
 
 class ViewQuestionsTest extends TestCase
 {
@@ -37,5 +38,26 @@ class ViewQuestionsTest extends TestCase
         $test->assertStatus(200)
             ->assertSee($question->title)
             ->assertSee($question->content);
+    }
+
+    /** @test */
+    public function user_can_view_a_published_question()
+    {
+        $question = factory(Question::class)->create(['published_at' => Carbon::parse('-1 week')]);
+
+        $this->get('/questions/' . $question->id)
+            ->assertStatus(200)
+            ->assertSee($question->title)
+            ->assertSee($question->content);
+    }
+
+    /** @test */
+    public function user_cannot_view_unpublished_question()
+    {
+        $question = factory(Question::class)->create(['published_at' => null]);
+
+        $this->withExceptionHandling()
+            ->get('/questions/' . $question->id)
+            ->assertStatus(404);
     }
 }
