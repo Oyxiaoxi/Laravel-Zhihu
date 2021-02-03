b762d25a7a7c1f12e39150a8d4f7e19f2c78d226
Oyxiaoxi <Oyxiaoxi@hotmail.com>
2021-02-03 17:24:16 +0000

    Post Answer Validation:part 1

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
 
 Route::get('/questions', 'QuestionsController@index');
 Route::get('/questions/{question}', 'QuestionsController@show');
 
 Route::post('/questions/{question}/answers', 'AnswersController@store');
+
